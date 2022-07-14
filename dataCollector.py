43400778df51a70d77feb1a366148361730e979d
Valentina Varela Alzate <valentina.varela@utp.edu.co>
2022-07-13 23:35:14 -0500

    normalizacion de la informacion

--- dataCollector.py
+++ dataCollector.py
@@ -1,31 +1,100 @@
 import requests
 import os
 from datetime import date
+import pandas as pd
 
 
 def obtener_datos(url, categoria):
     
     informacion = requests.get(url) 
     
-    today = date.today()  # Date of file download
+    # Date of file download
+    today = date.today()  
     current_date = today.strftime("%d-%m-%Y")
     year_month = today.strftime('%Y-%b')
     
+    # Folder creation
     carpeta = categoria + '/' + year_month
     if not os.path.exists(carpeta):
         os.makedirs(carpeta)
     
-    archivo = open(categoria 
-        + '/'
-        + year_month
-        + '/' 
-        + categoria 
-        + '-' 
-        + current_date 
-        + '.csv', 'wb')
+    # File script
+    subcarpeta = categoria + '/' + year_month + '/' + categoria + '-' + current_date + '.csv'
+    archivo = open(subcarpeta, 'wb')
     
     archivo.write(informacion.content)
     archivo.close()
+    
+    return subcarpeta
+    
+    
+def procesar_datos(museos,cines,bibliotecas):
+    
+    df_museos = pd.read_csv(museos,encoding='UTF-8')
+    df_cines = pd.read_csv(cines,encoding='UTF-8')
+    df_bibliotecas = pd.read_csv(bibliotecas,encoding='UTF-8')
+    
+    #creo una variable con las columnas de interés
+    cols_interes = ['cod_localidad', 'id_provincia', 'id_departamento',
+                    'categoría', 'provincia', 'localidad', 'nombre',
+                    'domicilio', 'código postal', 'número de teléfono',
+                    'mail', 'web']
+
+
+    # Normalization of the information by changing the names of the columns 
+    # to create the single table
+    
+    df_museos.rename(columns = {'Cod_Loc' : 'cod_localidad',
+                                'IdProvincia' : 'id_provincia',
+                                'IdDepartamento' : 'id_departamento',
+                                'categoria' : 'categoría',
+                                'direccion' : 'domicilio',
+                                'CP' : 'código postal',
+                                'telefono' : 'número de teléfono',
+                                'Mail' : 'mail',
+                                'Web' : 'web'},
+                    inplace = True)
+
+    df_cines.rename(columns = {'Cod_Loc' : 'cod_localidad',
+                                    'IdProvincia' : 'id_provincia',
+                                    'IdDepartamento' : 'id_departamento',
+                                    'Categoría' : 'categoría', 
+                                    'Provincia' : 'provincia',
+                                    'Localidad' : 'localidad',
+                                    'Nombre' : 'nombre',
+                                    'Domicilio' : 'domicilio',
+                                    'CP' : 'código postal',
+                                    'Fuente':'fuente',
+                                    'Teléfono' : 'número de teléfono',
+                                    'Mail' : 'mail',
+                                    'Web' : 'web'},
+                        inplace = True)
+
+    df_bibliotecas.rename(columns = {'Cod_Loc' : 'cod_localidad',
+                                    'IdProvincia' : 'id_provincia',
+                                    'IdDepartamento' : 'id_departamento',
+                                    'Categoría' : 'categoría', 
+                                    'Provincia' : 'provincia',
+                                    'Localidad' : 'localidad',
+                                    'Nombre' : 'nombre',
+                                    'Domicilio' : 'domicilio',
+                                    'CP' : 'código postal', 
+                                    'Fuente':'fuente',
+                                    'Teléfono' : 'número de teléfono',
+                                    'Mail' : 'mail',
+                                    'Web' : 'web'},
+                        inplace = True)
+    
+    # data frame con el total de columnas
+    tabla_unica = pd.concat([df_museos, df_cines, df_bibliotecas])
+
+    #data frame solo con las columnas de interes a normalizar
+    tabla_unica = tabla_unica.loc[:, cols_interes]    #contiene solo las columnas de interes a normalizar
+    tabla_unica['fecha_de_carga'] = date.today()
+    
+    print(date.today())
+    print(tabla_unica)
+    
     
     
     
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-'''
+"""
 CHALLENGE DATA ANALYTICS CON PYTHON
 Author: Valentina Varela Alzate
-'''
+"""
 
 import userInterface
 import dataCollector
@@ -11,31 +11,20 @@ def run():
     
     userInterface.menu_principal()
     
-    museos = '''
-            https://datos.gob.ar/dataset/cultura-mapa-cultural
-            -espacios-culturales/archivo/cultura_4207def0-2ff7
-            -41d5-9095-d42ae8207a5d
-            '''
+    museos = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv'
     
-    cines = ''' 
-            https://datos.gob.ar/dataset/cultura-mapa-cultural
-            -espacios-culturales/archivo/cultura_392ce1a8-ef11
-            -4776-b280-6f1c7fae16ae
-            '''
+    cines = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv'
     
-    bibliotecas = '''
-                https://datos.gob.ar/dataset/cultura-mapa
-                -cultural-espacios-culturales/archivo/cultura
-                _01c6c048-dbeb-44e0-8efa-6944f73715d7
-    
-                '''
+    bibliotecas = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
  
     # Reading and storing data
-    dataCollector.obtener_datos(museos,"museos")
-    dataCollector.obtener_datos(cines,"cines")
-    dataCollector.obtener_datos(bibliotecas,"bibliotecas")
+    museos_csv = dataCollector.obtener_datos(museos,'museos')
+    cines_csv = dataCollector.obtener_datos(cines,'cines')
+    bibliotecas_csv = dataCollector.obtener_datos(bibliotecas,'bibliotecas')
     
-
+    # Data processing and table normalization
+    dataCollector.procesar_datos(museos_csv,cines_csv,bibliotecas_csv)
+    
 
 if __name__ == '__main__':
     run()
