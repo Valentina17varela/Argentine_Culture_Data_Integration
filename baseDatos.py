a065a8eb7229e1b09c220b713bd58e579fdbfde5
Valentina Varela Alzate <valentina.varela@utp.edu.co>
2022-07-23 15:05:27 -0500

    implementacion de logging y decouple

--- baseDatos.py
+++ baseDatos.py
@@ -1,5 +1,4 @@
 import pandas as pd
-import tabulate
 import sqlalchemy
 
 
--- main.py
+++ main.py
@@ -6,40 +6,55 @@ Author: Valentina Varela Alzate
 import userInterface
 import dataCollector
 import baseDatos
+import logging
 from sqlalchemy import create_engine
 from sqlalchemy_utils import create_database
 from sqlalchemy_utils import database_exists
+from decouple import config
+
+
+MUSEOS = config('URL_MUSEOS')
+CINES = config('URL_CINES')
+BIBLIOTECAS = config('URL_BIBLIOTECAS')
+
+HOST = config('POSTGRES_HOST')
+PORT = config('POSTGRES_PORT')
+PASS = config('POSTGRES_PASSWORD')
+USER = config('POSTGRES_USER')
+NAME = config('POSTGRES_DB')
 
 
 def run():
     
+    logging.info("Inicio del programa, accediendo a las fuentes de informacion")
+    
     userInterface.menu_principal()
     
-    museos = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/4207def0-2ff7-41d5-9095-d42ae8207a5d/download/museos_datosabiertos.csv'
-    
-    cines = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/392ce1a8-ef11-4776-b280-6f1c7fae16ae/download/cine.csv'
-    
-    bibliotecas = 'https://datos.cultura.gob.ar/dataset/37305de4-3cce-4d4b-9d9a-fec3ca61d09f/resource/01c6c048-dbeb-44e0-8efa-6944f73715d7/download/biblioteca_popular.csv'
- 
     # Reading and storing data
-    museos_csv = dataCollector.obtener_datos(museos,'museos')
-    cines_csv = dataCollector.obtener_datos(cines,'cines')
-    bibliotecas_csv = dataCollector.obtener_datos(bibliotecas,'bibliotecas')
+    museos_csv = dataCollector.obtener_datos(MUSEOS,'museos')
+    cines_csv = dataCollector.obtener_datos(CINES,'cines')
+    bibliotecas_csv = dataCollector.obtener_datos(BIBLIOTECAS,'bibliotecas')
+    logging.info("Se almacena de forma local la informacion obtenida")
     
     # Data processing and table normalization
     unica, conjuntos, info_cines = dataCollector.procesar_datos(museos_csv,cines_csv,bibliotecas_csv)
 
     userInterface.presentacion_tablas();
+    logging.info("Procesamiento de datos y normalizacion de tablas")
 
     # Creation of tables in the database and visualization
-    db = 'postgresql+psycopg2://postgres:admin123@localhost:1717/informacionCultural'
+    db = 'postgresql://'+USER+':'+PASS+'@'+HOST+':'+PORT+'/'+NAME
     if not database_exists(db):
         create_database(db)
     SQLengine = create_engine(db)
+    logging.info("Conexion exitosa a la base de datos")
     
     baseDatos.creacion_DB(unica,"Tabla normalizada",SQLengine)
     baseDatos.creacion_DB(conjuntos,"Datos conjuntos",SQLengine)
     baseDatos.creacion_DB(info_cines,"Informacion de cines",SQLengine)
+    logging.info("Tablas cargadas a la base de datos")
+    
+    logging.info("Ejecucion finalizada")
     
     
 if __name__ == '__main__':
